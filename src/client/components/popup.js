c721f8d2ce879825348ad9709b1c0a330307844f
maelletc <maelle.stojanovic@insa-lyon.fr>
2023-06-14 09:16:38 +0200

    k

--- src/client/components/popup.js
+++ src/client/components/popup.js
@@ -7,16 +7,11 @@ const authToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0NmYxMTU0Yjgz
 const Popup = props => {
   const [textboxValue, setTextboxValue] = useState('');
   const [selectedFile, setSelectedFile] = useState('');
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
 
   const handleFileUpload = (event) => {
     const file = event.target.files[0];
-    const formData = new FormData();
-    formData.append("picture", file);
-    formData.append("userId", "6475faae93a9ea8ff1cec1cf");
-    formData.append("timeEnd", Date.now() + 3 * 60000);
-    formData.append("description", textboxValue);
-    formData.append("picturePath", file.name);
-
     setSelectedFile(file);
   };
 
@@ -24,18 +19,24 @@ const Popup = props => {
     setTextboxValue(event.target.value);
   };
 
-  const handleClick = (e) => {
-    e.preventDefault();
-    const descInput = document.getElementById("desc-input");
-    if (descInput) {
-      setTextboxValue(descInput.value);
-    }
+  const handleHoursChange = (event) => {
+    const value = parseInt(event.target.value);
+    setHours(value);
+  };
 
-    // Perform any necessary actions when the button is clicked
+  const handleMinutesChange = (event) => {
+    const value = parseInt(event.target.value);
+    setMinutes(value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const durationInMilliseconds = (hours * 60 * 60 * 1000) + (minutes * 60 * 1000);
+
     const formData = new FormData();
     formData.append("picture", selectedFile);
     formData.append("userId", "6475faae93a9ea8ff1cec1cf");
-    formData.append("timeEnd", Date.now() + 3 * 60000);
+    formData.append("timeEnd", Date.now() + durationInMilliseconds);
     formData.append("description", textboxValue);
     formData.append("picturePath", selectedFile.name);
 
@@ -66,9 +67,20 @@ const Popup = props => {
         ) : (
           <p>No file uploaded</p>
         )}
-        <form>
+        <form onSubmit={handleSubmit}>
           Description<br /><textarea type="text" name="description" className='desc' id="desc-input" onChange={handleDescChange}></textarea><br /><br />
-          <button value='upload' className='btn' onClick={handleClick}>Poster</button>
+          Choose the duration of your post<br/><br/>
+          <label>
+            Hours:
+            <input type="number" value={hours} onChange={handleHoursChange} min={0} className="time"/>
+          </label>
+          <br /><br />
+          <label>
+            Minutes:
+            <input type="number" value={minutes} onChange={handleMinutesChange} min={0} max={59} className="time"/>
+          </label>
+          <br /><br />
+          <button value='upload' className='btn' type="submit">Post</button>
         </form>
       </div>
     </div>
